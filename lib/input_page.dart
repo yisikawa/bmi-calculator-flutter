7100c5966f11e84126700f275d4fb75e1ce727a6
Yoshiaki Ishikawa <yisikawa@ma.medias.ne.jp>
2020-03-06 18:05:24 +0900

    change slider thema inactivecolor

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -95,6 +95,7 @@ class _InputPageState extends State<InputPage> {
                     SliderTheme(
                       data: SliderTheme.of(context).copyWith(
                         activeTrackColor: Colors.white,
+                        inactiveTrackColor: Color(0xFF8D8E98),
                         thumbColor: Color(0xFFEB1555),
                         overlayColor: Color(0x40EB1555),
                         thumbShape:
@@ -106,7 +107,6 @@ class _InputPageState extends State<InputPage> {
                         value: height.toDouble(),
                         min: 120.0,
                         max: 220.0,
-                        inactiveColor: Color(0xFF8D8E98),
                         onChanged: (double newValue) {
                           setState(() {
                             height = newValue.round();
